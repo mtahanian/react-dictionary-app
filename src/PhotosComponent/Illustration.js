5499c23977f3731925e1f1da957661757f0a309c
mtahanian <35131374+mtahanian@users.noreply.github.com>
2022-08-02 20:29:59 +0430

    Update Illustration.js

--- src/PhotosComponent/Illustration.js
+++ src/PhotosComponent/Illustration.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import illustration from "../images/girl.svg";
+import illustration from "../images/Untitled-1 copy.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
